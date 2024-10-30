7d4e836e44f2ac8e74ae7faf2c9582c783471a3b
Filippo Toso <filippo.toso@creativepark.it>
2024-10-30 18:06:04 +0100

    Improve IsResource trait

--- src/Models/Concerns/IsResource.php
+++ src/Models/Concerns/IsResource.php
@@ -6,6 +6,7 @@ use FilippoToso\ResourcePermissions\Support\Helper;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Query\JoinClause;
+use Illuminate\Support\Facades\DB;
 
 /**
  * Trait for your Resouce models to get users models
@@ -16,14 +17,18 @@ trait IsResource
     {
         $rolesIds = is_null($role) ? null : Helper::getRolesIds($role);
 
-        /** @var Model $this */
-        $query->join(config('resource-permissions.tables.role_user'), function (JoinClause $join) use ($user, $rolesIds) {
-            $join->on(config('resource-permissions.tables.role_user').'.resource_id', '=', $this->getTable().'.'.$this->getKeyName())
-                ->where(config('resource-permissions.tables.role_user').'.resource_type', '=', $this->getMorphClass())
-                ->where(config('resource-permissions.tables.role_user').'.user_id', '=', $user->getKey())
-                ->where(config('resource-permissions.tables.role_user').'.user_type', '=', $user->getMorphClass())
-                ->when(! is_null($rolesIds), function (JoinClause $join) use ($rolesIds) {
-                    $join->whereIn(config('resource-permissions.tables.role_user').'.role_id', $rolesIds);
+        $query->whereExists(function ($query) use ($user, $rolesIds) {
+            /** @var Model $this */
+            $query->select(DB::raw(1))
+                ->from($this->getTable())
+                ->join(config('resource-permissions.tables.role_user'), function (JoinClause $join) use ($user, $rolesIds) {
+                    $join->on(config('resource-permissions.tables.role_user') . '.resource_id', '=', $this->getTable() . '.' . $this->getKeyName())
+                        ->where(config('resource-permissions.tables.role_user') . '.resource_type', '=', $this->getMorphClass())
+                        ->where(config('resource-permissions.tables.role_user') . '.user_id', '=', $user->getKey())
+                        ->where(config('resource-permissions.tables.role_user') . '.user_type', '=', $user->getMorphClass())
+                        ->when(! is_null($rolesIds), function (JoinClause $join) use ($rolesIds) {
+                            $join->whereIn(config('resource-permissions.tables.role_user') . '.role_id', $rolesIds);
+                        });
                 });
         });
     }
@@ -32,12 +37,16 @@ trait IsResource
     {
         $rolesIds = is_null($role) ? null : Helper::getRolesIds($role);
 
-        /** @var Model $this */
-        $query->join(config('resource-permissions.tables.role_user'), function (JoinClause $join) use ($rolesIds) {
-            $join->on(config('resource-permissions.tables.role_user').'.resource_id', '=', $this->getTable().'.'.$this->getKeyName())
-                ->where(config('resource-permissions.tables.role_user').'.resource_type', '=', $this->getMorphClass())
-                ->when(! is_null($rolesIds), function (JoinClause $join) use ($rolesIds) {
-                    $join->whereIn(config('resource-permissions.tables.role_user').'.role_id', $rolesIds);
+        $query->whereExists(function ($query) use ($rolesIds) {
+            /** @var Model $this */
+            $query->select(DB::raw(1))
+                ->from($this->getTable())
+                ->join(config('resource-permissions.tables.role_user'), function (JoinClause $join) use ($rolesIds) {
+                    $join->on(config('resource-permissions.tables.role_user') . '.resource_id', '=', $this->getTable() . '.' . $this->getKeyName())
+                        ->where(config('resource-permissions.tables.role_user') . '.resource_type', '=', $this->getMorphClass())
+                        ->when(! is_null($rolesIds), function (JoinClause $join) use ($rolesIds) {
+                            $join->whereIn(config('resource-permissions.tables.role_user') . '.role_id', $rolesIds);
+                        });
                 });
         });
     }
