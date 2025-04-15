b333d9695c8f4bb4fd463346229d8fef5071ba23
Filippo Toso <filippo.toso@creativepark.it>
2025-04-15 15:57:15 +0200

    feat: add User::whereHasRoleWithResource() scope

--- src/Models/Concerns/Support/HasRoles.php
+++ src/Models/Concerns/Support/HasRoles.php
@@ -6,6 +6,7 @@ use FilippoToso\ResourcePermissions\Finders\Finder;
 use FilippoToso\ResourcePermissions\Models\Pivots\RoleUserPivot;
 use FilippoToso\ResourcePermissions\Support\Helper;
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
+use Illuminate\Database\Eloquent\Builder;
 
 /**
  * Trait for your User models to add roles.
@@ -17,6 +18,26 @@ trait HasRoles
         return $this->morphToMany(config('resource-permissions.models.role'), 'user', config('resource-permissions.tables.role_user'))
             ->withPivot('resource_id', 'resource_type')
             ->using(RoleUserPivot::class);
+    }
+
+    public function scopeWhereHasRoleWithResource(Builder $query, $role, $resource, $or = true)
+    {
+        $rolesIds = Helper::getRolesIds($role);
+        $resources = is_null($resource) ? [$resource] : Helper::getResources($resource);
+
+        $table = config('resource-permissions.tables.role_user');
+
+        $query->whereHas('roles', function ($query) use ($rolesIds, $resources, $or, $table) {
+            $query->whereIn($table . '.role_id', $rolesIds)
+                ->where(function ($query) use ($resources, $or, $table) {
+                    $where = ($or) ? 'orWhere' : 'where';
+
+                    foreach ($resources as $resource) {
+                        $query->{$where}($table . '.resource_type', '=', $resource->type)
+                            ->{$where}($table . '.resource_id', '=', $resource->id);
+                    }
+                });
+        });
     }
 
     public function hasRole($role, $resource = null, $or = true, $strict = true)
